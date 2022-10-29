65fc7534df4680a8db5013725397e6341cf07b66
xiaoyan <94773912+xymasszs@users.noreply.github.com>
2022-10-29 15:58:04 +0800

    github update

--- src/main/java/com/massz/TestGit.java
+++ src/main/java/com/massz/TestGit.java
@@ -10,5 +10,6 @@ public class TestGit {
         System.out.println("hot-fix test   ");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("git hub update");
     }
 }
