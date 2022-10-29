a7c578ead96de8f777be480833c39c63cf558941
sch <sch@qq.com>
2022-10-29 14:09:09 +0800

    second commit

--- src/main/java/com/massz/TestGit.java
+++ src/main/java/com/massz/TestGit.java
@@ -3,5 +3,6 @@ package com.massz;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2 ");
     }
 }
